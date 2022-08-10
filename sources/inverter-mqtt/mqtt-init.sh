e03505c0d29344a000275424f823e23675575977
AndyRap <andy@the-hodges.com>
2022-08-10 10:47:06 +0100

    Update mqtt-init.sh
    Changed "Mode" topic to "Inverter_mode" to match settings used in mqtt-push.sh

--- sources/inverter-mqtt/mqtt-init.sh
+++ sources/inverter-mqtt/mqtt-init.sh
@@ -104,7 +104,7 @@ registerTopic "Load_watt" "W" "chart-bell-curve" "power"
 registerEnergyTopic "Load_watthour" "Wh" "chart-bell-curve" "energy"
 registerTopic "Max_charge_current" "A" "current-ac" "current"
 registerTopic "Max_grid_charge_current" "A" "current-ac" "current"
-registerTopic "Mode" "" "solar-power" "" # 1 = Power_On, 2 = Standby, 3 = Line, 4 = Battery, 5 = Fault, 6 = Power_Saving, 7 = Unknown
+registerTopic "Inverter_mode" "" "solar-power" "" # 1 = Power_On, 2 = Standby, 3 = Line, 4 = Battery, 5 = Fault, 6 = Power_Saving, 7 = Unknown
 registerTopic "Out_source_priority" "" "grid" "none"
 registerTopic "PV_in_current" "A" "solar-panel-large" "current"
 registerTopic "PV_in_voltage" "V" "solar-panel-large" "voltage"
